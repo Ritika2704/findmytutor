508ff643741042a453d595f7f574cd4405bb85e9
Ritika <ritika@ritisinha272.com>
2022-07-26 17:46:38 +0530

    1) Added Login Controller and LoginPage View 2) Made minor changes in _Layout.cshtml 3) Removed Contact page

--- findmytutor/Controllers/HomeController.cs
+++ findmytutor/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace findmytutor.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
